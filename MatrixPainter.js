3c6680fca2158e681d1b15d26e0023840e9a7652
Alexey Veprev <aveprev27@gmail.com>
2016-10-07 18:03:58 +0300

    Initial commit
    Skeleton of the project and matrix painter were implemented.

---
+++ MatrixPainter.js
@@ -0,0 +1,69 @@
+"use strict"
+
+function MatrixPainter(canvas, matrix, width, height) {
+	self = this;
+
+	function drawGrid() {
+		context.strokeStyle = "#000";    // Line color.
+		// Draw vertical lines.
+		for (var i = 0; i <= size.x; i++) {
+			context.beginPath();
+			context.moveTo(i * cell.width, 0);
+			context.lineTo(i * cell.width, height);
+			context.stroke();
+		}
+
+		// Draw horizontal lines.
+		for (var i = 0; i <= size.y; i++) {
+			context.beginPath();
+			context.moveTo(0, i * cell.height);
+			context.lineTo(width, i * cell.height);
+			context.stroke();
+		}
+	}
+
+	function drawActiveElements() {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.fillStyle   = "#44AA22"; // Fill active cell color.
+		for (var i = 0; i < size.x; i++) {
+			for (var j = 0; j < size.y; j++) {
+				if (self.matrix[i][j] == 1) {
+					context.fillRect(j * cell.width,
+									 i * cell.height,
+									 cell.width,
+									 cell.height);
+				}
+			}
+		}
+	}
+
+	self.getPositionFromCoord = function(x, y) {
+		return {x: Math.floor(x / cell.width), y: Math.floor(y / cell.height)};
+	}
+	
+	self.update = function(matrix) {
+		self.matrix = matrix;
+		drawActiveElements();
+		drawGrid();
+	}
+
+	self = this;
+	self.width = width;
+	self.height = height;
+	self.matrix = matrix;
+
+	var size = new Object();
+	size.x = matrix[0].length;
+	size.y = matrix.length;
+
+	var cell = new Object();
+	cell.width = width / size.x;
+	cell.height = height / size.y;
+
+	var context = canvas.getContext("2d");
+	canvas.width  = width;
+	canvas.height = height;
+
+	drawActiveElements();
+	drawGrid();
+}
---
+++ game_of_life.js
@@ -0,0 +1,63 @@
+"use strict"
+
+var world = null;
+
+function World(sizeX, sizeY, width, height) {
+	var self = this;
+	self.matrix = [];
+	self.width = width;
+	self.height = height;
+
+	// Initiate world with zeros.
+	for (var i = 0; i < sizeX; i++) {
+		var row = [];
+		for (var j = 0; j < sizeY; j++) {
+			row.push(0);
+		}
+		self.matrix.push(row);
+	}
+	
+	var painter = new MatrixPainter(document.getElementById("canvas"), self.matrix, width, height);
+
+	self.draw = function() {
+		painter.update(self.matrix);
+	}
+
+	self.handleClick = function(x, y) {
+		var pos = painter.getPositionFromCoord(x, y);
+		self.matrix[pos.y][pos.x] = Math.abs(self.matrix[pos.y][pos.x] - 1);
+		painter.update(self.matrix);
+	}
+
+}
+
+function createWorld() {
+	world = new World(20, 20, 800, 800);
+	world.draw();
+}
+
+function relMouseCoords(event) {
+	var totalOffsetX = 0;
+	var totalOffsetY = 0;
+	var canvasX = 0;
+	var canvasY = 0;
+	var currentElement = this;
+
+	do {
+		totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+		totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+	}
+	while (currentElement = currentElement.offsetParent)
+
+	canvasX = event.pageX - totalOffsetX;
+	canvasY = event.pageY - totalOffsetY;
+
+	return {x:canvasX, y:canvasY}
+}
+HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
+
+function handleClick(event) {
+	var canvas = document.getElementById("canvas");
+	var coords = canvas.relMouseCoords(event);
+	world.handleClick(coords.x, coords.y);
+}
