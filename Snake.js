eb1274d34af7893de3850c57dc27eb8f627b505b
Alexey Veprev <aveprev27@gmail.com>
2016-10-10 23:42:32 +0300

    Snake: implement step()

--- Snake.js
+++ Snake.js
@@ -7,8 +7,17 @@ function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+function replaceBiggerNeighbor(matrix) {
+    
+}
+
 function Snake(canvas, sizeX, sizeY, width, height) {
   var self = this;
+
+  var Direction = Object.freeze({UP:    {x:  0,  y: -1},
+                                 DOWN:  {x:  0,  y:  1},
+                                 RIGHT: {x:  1,  y:  0},
+                                 LEFT:  {x: -1,  y:  0}});
 
   self.width = {};
   self.width.pixel = width;
@@ -47,8 +56,10 @@ function Snake(canvas, sizeX, sizeY, width, height) {
     head.x = Math.floor(self.width.num / 2);
     head.y = Math.floor(self.height.num / 2);
 
+    head.direction = Direction.UP;
+
     field[head.y][head.x] = 2;
-    field[head.y - 1][head.x] = 1; // Tail.
+    field[head.y + 1][head.x] = 1; // Tail.
   }
 
   function placeFood() {
@@ -64,6 +75,65 @@ function Snake(canvas, sizeX, sizeY, width, height) {
       }
     }
   }
+
+  function checkNewHeadPos(coord) {
+    if (coord.x < 0 || coord.x >= self.width.num ||
+        coord.y < 0 || coord.y >= self.height.num) {
+      return false;
+    }
+
+    if (field[coord.y][coord.x] > 0) {
+      return false;
+    }
+
+    return true;
+  }
+
+  self.step = function() {
+    var newHeadPos = {x: head.x + head.direction.x,
+                      y: head.y + head.direction.y};
+
+    if (!checkNewHeadPos(newHeadPos)) {
+      //self.gameOverCallback();
+      alert("Game over");
+      return;
+    }
+
+    var food = Math.abs(field[newHeadPos.y][newHeadPos.x]);
+
+    field[newHeadPos.y][newHeadPos.x] = field[head.y][head.x] + 1;
+    head.x = newHeadPos.x;
+    head.y = newHeadPos.y;
+
+    var x = head.x;
+    var y = head.y;
+    var direction;
+    while (true) {
+      if (y - 1 >= 0 && field[y - 1][x] == field[y][x] - 1) {
+        direction = Direction.UP;
+      }
+      else if (y + 1 < self.height.num && field[y + 1][x] == field[y][x] - 1) {
+        direction = Direction.DOWN;
+      }
+      else if (x + 1 < self.width.num && field[y][x + 1] == field[y][x] - 1) {
+        direction = Direction.RIGHT;
+      }
+      else if (x - 1 >= 0 && field[y][x - 1] == field[y][x] - 1) {
+        direction = Direction.LEFT;
+      }
+      else {
+        field[y - direction.y][x - direction.x] += food;
+        break;
+      }
+
+      field[y][x] = field[y + direction.y][x + direction.x] + food;
+      y = y + direction.y;
+      x = x + direction.x;
+    }
+
+    self.draw();
+  }
+
 }
 
 function startSnake(canvas, sizeX, sizeY) {
