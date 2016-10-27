7ab8d943133c5905535deb773973243d7d1badea
Alexey Veprev <aveprev27@gmail.com>
2016-10-27 23:07:49 +0300

    Grunt: update gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,21 @@
 module.exports = function(grunt) {
+  var path = {src:   'src/',
+              build: 'build/',
+              js:    'js/*.js',
+              html:  '*.html',
+              css:   'styles/*.css'}
+
   grunt.initConfig({
     jshint: {
-      all: ['src/js/*.js']
+      all: path.src + path.js,
     },
     uglify: {
       build: {
         files: [{
             expand: true,
-            src: ['src/js/*.js'],
-            dest: 'build/'
+            src:  path.js,
+            dest: path.build,
+            cwd:  path.build
         }]
       }
     },
@@ -16,17 +23,31 @@ module.exports = function(grunt) {
       build: {
         files: [{
             expand: true,
-            src: ['src/*.html', 'src/styles/*.css'],
-            dest: 'build/'
+            src: [path.html, path.css],
+            dest: path.build,
+            cwd:  path.src
         }]
       }
+    },
+    strip_code: {
+      options: {
+        start_comment: "test-code",
+        end_comment: "end-test-code",
+      },
+      your_target: {
+        expand: true,
+        src:  path.js,
+        dest: path.build,
+        cwd:  path.src
     }
+  }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-strip-code');
 
-  grunt.registerTask('default', ['jshint', 'uglify', 'copy']);
+  grunt.registerTask('default', ['jshint', 'strip_code', 'uglify', 'copy']);
   grunt.registerTask('build', 'default');
 };
